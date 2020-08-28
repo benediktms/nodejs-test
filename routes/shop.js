8a4945128caa143c4d49766d471f7ca7f5620737
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-28 10:52:12 +0100

    added rootDir helper util

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir = require('../util/path');
 
 router.get('/', (req, res, next) => {
   // __dirname is a global variabel that detects the OS and builds an absolute path
   // the path module allows us to concatenate the values to build a correct path irregardless fo the OS
   // we can concatonate directory navigation as well
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  // here we created the rootDir helper utility to get the root directory for us automatically
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
