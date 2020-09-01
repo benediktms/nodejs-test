b65000debd286ef2ddc1eb1e51bbef5a5b602cee
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-09-01 12:14:46 +0100

    adding templating engines

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 // Local files
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   // __dirname is a global variabel that detects the OS and builds an absolute path
   // the path module allows us to concatenate the values to build a correct path irregardless fo the OS
   // we can concatonate directory navigation as well
   // here we created the rootDir helper utility to get the root directory for us automatically
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
